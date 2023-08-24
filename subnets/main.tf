7dfff8c8b5904af2df2502b3f667b32b87e02443
lip2050 <lip2050@users.noreply.github.com>
2023-08-23 22:57:00 -0400

    Terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "main" {
   cidr_block = element(var.cidr_block, count.index )
 
   tags = merge ({
-    Name = "${var.env}-${var.subnet_name}-web-subnet"
+    Name = "${var.env}-${var.subnet_name}-subnet"
   },
     var.tags)
 }
